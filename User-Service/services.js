1ba40e32282a0830a0f52a1fd54b2b1d890a8c0e
Tejas Agrawal <tejasagrawal36@gmail.com>
2025-04-02 15:34:32 +0530

    start setup Inventory Service

--- User-Service/services.js
+++ User-Service/services.js
@@ -58,5 +58,5 @@ app.post('/login', async (req, res) => {
     res.json({ token });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`User Service running on port ${PORT}`));
